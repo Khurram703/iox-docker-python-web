b46c0db5ea10b8417fc1a8b68b3fc330ebc4ba86
JockDaRock <jock.da.rock@gmail.com>
2019-06-06 14:10:31 -0500

    Docker label updates and added IOx logging.
    Signed-off-by: JockDaRock <jock.da.rock@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,18 @@ EXPOSE 8000
 
 COPY main.py /main.py
 
+LABEL cisco.info.name="iox_docker_python_web" \
+      cisco.info.description="Basic WebApp for IOx" \
+      cisco.info.version="0.1" \
+      cisco.info.author-link="" \
+      cisco.info.author-name="" \
+      cisco.type=docker \
+      cisco.cpuarch=x86_64 \
+      cisco.resources.profile=custom \
+      cisco.resources.cpu=100 \
+      cisco.resources.memory=100 \
+      cisco.resources.disk=10 \
+      cisco.resources.network.0.interface-name=eth0 \
+      cisco.resources.network.0.ports.tcp=[8000]
+
 CMD python3 /main.py
--- main.py
+++ main.py
@@ -1,15 +1,52 @@
-from bottle import route, run
+from bottle import Bottle, request, response
 from datetime import datetime
+from functools import wraps
+import logging
+import os
+
+try:
+    directory = os.environ['CAF_APP_LOG_DIR'] + "/"
+except KeyError as e:
+    # print(str(e) + " is non-existent")
+    directory = "./"
+
+logger = logging.getLogger('webapp')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(directory + 'webapp.log')
+formatter = logging.Formatter('%(msg)s')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
-@route('/status/<device_id>')
+def log_to_logger(fn):
+    @wraps(fn)
+    def _log_to_logger(*args, **kwargs):
+        request_time = datetime.now()
+        actual_response = fn(*args, **kwargs)
+        logger.info('%s %s %s %s %s' % (
+            request.remote_addr,
+            request_time,
+            request.method,
+            request.url,
+            response.status))
+        return actual_response
+    return _log_to_logger
+
+
+app = Bottle()
+app.install(log_to_logger)
+
+
+@app.route('/status/<device_id>')
 def status(device_id):
-    return { "system": 1, "device": str(device_id) }
+    return {"system": 1, "device": str(device_id)}
 
 
-@route('/time')
+@app.route('/time')
 def time():
     current_time = datetime.now().isoformat(' ')
     return {"system": 1, "datetime": current_time}
 
-run(host='0.0.0.0', port=8000)
+
+app.run(host='0.0.0.0', port=8000, quiet=True)
